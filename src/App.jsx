04fec474e6b29f2378bd2f797142d10228cb23ef
praff1351 <prafullihoo.7777@gmail.com>
2025-10-10 04:11:56 +0530

    Created Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import {React, BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
-    <div className='text-green-500'>
-    Testing tailwind
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/profile' element={<Profile />} />
+      
+    </Routes>
     
-    </div>
+    </BrowserRouter>
+
   )
 }
 
