c7daa839a605d53cf44485daa5eca7cf4cb7533f
praff1351 <prafullihoo.7777@gmail.com>
2025-10-11 03:19:39 +0530

    completed signing in part

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async(req, res, next)=>{
   const {username, email, password} = req.body;
@@ -14,7 +16,34 @@ export const signup = async(req, res, next)=>{
     
   } catch (error) {
     next(error);
-    
+
   }
 
+};
+
+
+export const signin = async(req, res, next)=>{
+  const {email, password} = req.body;
+  try {
+    const validUser = await User.findOne({email});
+    if(!validUser) return next(errorHandler(404, "User not found!"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if(!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+
+    const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET);
+    const {password: pass, ...rest} = validUser._doc; //displays everything except the password.
+    res
+    .cookie('access_token', token, {httpOnly:true})
+    .status(200)
+    .json(rest); //here rest of the things will get shown other than the password.
+
+  } catch (error) {
+    next(error); //from the middleware that we have created ourselves in index.js
+    
+  }
 }
+
+
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 
+
 const App = () => {
   return (
     <BrowserRouter>
