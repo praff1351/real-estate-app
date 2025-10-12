0b94d7e608dbe98523b52f129f53c84be470a573
praff1351 <prafullihoo.7777@gmail.com>
2025-10-12 21:38:06 +0530

    completed the profile page UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Search} from "lucide-react";
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const {currentUser} = useSelector(state => state.user);
   return (
 
     <header className='bg-slate-200 shadow-md'>
@@ -31,10 +33,17 @@ const Header = () => {
           <Link to={"/about"}>
           <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
           </Link>
-
-          <Link to={"/sign-in"}>
+          <Link to={"/profile"}>
+          {currentUser ? (
+            <img 
+            className='rounded-full size-7 object-cover'
+            src={currentUser.avatar} 
+            alt="profile" />
+          ):(
           <li className=' text-slate-700 hover:underline'>Sign In</li>
-          </Link>
+          )
+        }
+        </Link>
         </ul>
       </div>
     </header>
