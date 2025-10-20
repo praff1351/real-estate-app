fb33bbbb94d483cc6294cba7c577afdaa1713db5
praff1351 <prafullihoo.7777@gmail.com>
2025-10-20 23:26:31 +0530

    update Header Search form functionality

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -8,7 +8,7 @@ export default function Contact({ listing }) {
   const onChange = (e) => {
     setMessage(e.target.value);
   };
-
+  
   useEffect(() => {
     const fetchLandlord = async () => {
       try {
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Search} from "lucide-react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const Header = () => {
   const {currentUser} = useSelector(state => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
 
     <header className='bg-slate-200 shadow-md'>
@@ -17,12 +34,19 @@ const Header = () => {
       </h1>
         </Link>
 
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form
+        onClick={handleSubmit}
+        className='bg-slate-100 p-3 rounded-lg flex items-center'>
           <input 
           type="text" 
           placeholder="Search..."
-          className='bg-transparent focus:outline-none w-24 sm:w-64' />
+          className='bg-transparent focus:outline-none w-24 sm:w-64'
+          value={searchTerm}
+          onChange={(e)=> setSearchTerm(e.target.value)}         
+          />
+          <button className=''>
           <Search className='text-slate-600 ' />
+          </button>
         </form>
 
         <ul className='flex gap-4'>
